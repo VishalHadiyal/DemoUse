dc74c09cf6dcab20457bcb56513eb6932b7d7305
VishalHadiyal <vishalhadiyalefive@gmail.com>
2024-07-20 13:43:29 +0530

    modify code and remove plus sign

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-var usename = vishal_Hadiyal
-var password = test123
+var usename = "vishal_Hadiyal";
+var password = "test123";
 
-console.log(`${usename} + ${password}`);
+console.log(`${usename}${password}`);
