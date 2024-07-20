ef99aa0d772a14ee13027b3f450e4fdf0943bb74
VishalHadiyal <vishalhadiyalefive@gmail.com>
2024-07-20 13:55:10 +0530

    add two number program in login js file

--- login.js
+++ login.js
@@ -2,3 +2,20 @@ var usename = "vishal_Hadiyal";
 var password = "test123";
 
 console.log(`${usename}${password}`);
+
+
+// Add Two Number program.
+// Function to add two numbers
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+// Numbers to be added
+let number1 = 2000;
+let number2 = 5000;
+
+// Calculate the sum
+let sum = addNumbers(number1, number2);
+
+// Print the result
+console.log("The sum of " + number1 + " and " + number2 + " is: " + sum);
